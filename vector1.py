6d0131bfd36801fdb3d63f9fa9bd9e9e7ec8ddd7
Luis Mogro <luismogro65@gmail.com>
2025-08-11 11:20:12 -0300

    comienzo vector1

---
+++ vector1.py
@@ -0,0 +1,13 @@
+vector=[['17693604', 'kiko', True, True, 'ooperacion apendicitis', True, 'diabetes', True, 'metformina', True], [True, '', True, True, '4', True, '2', True, 'riesgo'], [True, True, True, True, True, True, True, True, True]]
+
+print(len(vector))
+
+baux=[]
+for  i in range(0,len(vector)):
+     aux=(vector[i])
+     for j in range(0,len(aux)):
+          baux.append(aux[j])
+
+print(baux)
+     
+           
