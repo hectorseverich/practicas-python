5f92518e5568621bf963afd11b900c23a4252f04
Luis Mogro <luismogro65@gmail.com>
2025-08-11 11:23:24 -0300

    comienzo a corregir temp.py

--- temp.py
+++ temp.py
@@ -3,19 +3,20 @@
 #filas = int(input("Introduce número de filas: "))
 #columnas = int(input("Introduce número de columnas: "))
 filas=2
-columnas=3
-
+columnas=2
 
 #matriz = [[float(input(f"Fila {i+1}, Columna {j+1}: ")) for j in range(columnas)] for i in range(filas)]
 
-matriz = [[float(input(f"dia{i+1}, mes{j+1}: ")) for j in range(columnas)] for i in range(filas)]
+matriz = [[int(input(f"dia{i+1}, mes{j+1}: ")) for j in range(columnas)] for i in range(filas)]
 
-may=[0]
+may=0
+
+print(matriz)
 
 for fila in matriz:
      for elemento in fila:
           if  elemento> may :
-               may=matriz[]
+               print(elemento)
 
      
 print("[", "   ".join(f"{elemento}" for elemento in fila), "]")
