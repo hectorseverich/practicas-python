381c49a4361c487a6cd9e24f8f4d98e15092b6fe
Luis Mogro <luismogro65@gmail.com>
2025-08-11 11:33:45 -0300

    comienzo a corregir pruebafor.py

---
+++ pruebafor.py
@@ -0,0 +1,8 @@
+numero_dni=20216625
+archivos_encontrados=['I:\imagen_scan\5\20216625_h1.pdf ']
+for  i in range(len(archivos_encontrados)):
+        aux=(archivos_encontrados[i])
+else:
+    print(f"No se encontraron archivos PDF para el DNI {numero_dni} en la subcarpeta correspondiente.")
+
+print(aux)
